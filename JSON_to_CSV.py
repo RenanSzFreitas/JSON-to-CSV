3858d30777b549d3e8172d824ca177dfd6f12a81
Renan Freitas <81188283+RenanSzFreitas@users.noreply.github.com>
2023-10-31 09:55:39 -0300

    Update JSON_to_CSV.py

--- JSON_to_CSV.py
+++ JSON_to_CSV.py
@@ -1,15 +1,15 @@
 import json
 import csv
 
-with open('arquivo.json', 'r') as arquivo_json: #Subistitua arquivo.json para o caminho do seu arquivo
-    dados = json.load(arquivo_json)
+with open('file.json', 'r') as file_json: #Replace file.json with the path to your file
+    data = json.load(file_json)
 
-with open('arquivo.csv', 'w', newline='') as arquivo_csv: #Subistitua arquivo.csv por um nome desejavel do seu arquivo CSV
-    escrever = csv.writer(arquivo_csv)
+with open('file.csv', 'w', newline='') as file_csv: #Replace file.csv with a desired name for your CSV file
+    writer = csv.writer(file_csv)
 
-    if dados:
-        cabecalhos = dados[0].keys()
-        escrever.writerow(cabecalhos)
+    if data:
+        headers = data[0].keys()
+        writer.writerow(headers)
 
-    for linha in dados:
-        escrever.writerow(linha.values())
+    for line in data:
+        writer.writerow(line.values())
